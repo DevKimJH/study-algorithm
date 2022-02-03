e84245b6314a2fd6132b3bf6f9bc4491ae842135
KJH <hg116701@naver.com>
2022-02-03 21:31:48 +0900

    예제 테스트케이스만 정답

---
+++ D220203/b1780종이의개수.java
@@ -0,0 +1,138 @@
+package studyAlgorithm.D220203;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 문제
+ N×N크기의 행렬로 표현되는 종이가 있다. 종이의 각 칸에는 -1, 0, 1 중 하나가 저장되어 있다. 우리는 이 행렬을 다음과 같은 규칙에 따라 적절한 크기로 자르려고 한다.
+
+ 만약 종이가 모두 같은 수로 되어 있다면 이 종이를 그대로 사용한다.
+ (1)이 아닌 경우에는 종이를 같은 크기의 종이 9개로 자르고, 각각의 잘린 종이에 대해서 (1)의 과정을 반복한다.
+ 이와 같이 종이를 잘랐을 때, -1로만 채워진 종이의 개수, 0으로만 채워진 종이의 개수, 1로만 채워진 종이의 개수를 구해내는 프로그램을 작성하시오.
+
+ * 입력
+ 첫째 줄에 N(1 ≤ N ≤ 37, N은 3k 꼴)이 주어진다. 다음 N개의 줄에는 N개의 정수로 행렬이 주어진다.
+
+ * 출력
+ *
+
+
+ */
+public class b1780종이의개수 {
+        static int[] answer = new int[3];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0 ; j < N ; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        process(arr, N, 0, 0);
+
+
+        for(int i = 0 ; i < 3 ; i++){
+            System.out.println(answer[i]);
+        }
+    }
+
+    static void process(int[][] arr, int N, int y, int x){
+
+        int num = arr[y][x];
+        boolean chkDiff = false;
+
+        for(int i = y ; i < y+N ; i++){
+            for(int j = x ; j < x+N ; j++){
+                if(num != arr[i][j]){
+                    chkDiff = true;
+                    break;
+                }
+            }
+
+            if(chkDiff) break;
+        }
+
+        if(chkDiff){
+            // y = 6
+            // x = 3, 4, 5 -> 3
+            // 4/3 * 3
+            // 5/3 * 3
+            // 3/3 * 3
+
+            // 6 = 6
+            // x = 6, 7, 8 -> 6
+            // 6/3 * 3
+
+            // 0 1 2 3 4 5 6 7 8 -> 0
+            // 9 10 11 12 13 14 15 16 17 => 9
+            // 10 / N *
+
+            /*
+               N이 9일때
+               0 1 2 -> 0
+               3 4 5 -> 3
+               6 7 8 -> 6
+
+               2/3 * 3 = 0
+               3/3 * 3 = 3
+               4/3 * 3 = 3
+               6/3 * 3 = 6
+               1 * 3
+               2 * 3
+
+               N이 27일때
+               0 1 2 3 4 5 6 7 8 -> 0
+
+               4 / 9 * 9 = 0
+               10 / 9 * 9 = 9
+
+
+               9 10 11 12 13 14 15 16 17 -> 9
+               18 19 20 21 22 23 24 25 26 -> 18
+            * */
+            for(int i = y ; i < y+3 ; i++){
+                for(int j = x ; j < x+3 ; j++){
+                    // 0, 0
+                    // 0, 3
+                    // 0, 6
+                    // 3, 0
+                    // 3, 3
+                    // 3, 6
+                    // 6, 0
+                    // 6, 3
+                    // 6, 6
+
+                    // 27
+                    // 0, 0
+                    // 0, 9
+                    // 0 11 -> 0 9
+                    // 0, 18
+                    // 9, 0
+                    // 9, 9
+                    // 9, 18
+                    // 18, 0
+                    // 18, 9
+                    // 18, 18
+
+
+                    //process(arr, N/3, i/(N/3) * N/3, j/(N/3) * N/3);
+                    process(arr, N/3, i * N/3, j * N/3);
+                }
+            }
+        }
+        else if(!chkDiff){
+            answer[num+1] += 1;
+        }
+    }
+}
+
