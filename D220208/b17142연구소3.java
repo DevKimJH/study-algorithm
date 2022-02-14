c78bfc48e1a47b546e1162200b18b3319fc65248
KJH <hg116701@naver.com>
2022-02-14 21:00:57 +0900

    연구소3 process 결과 지도 까지 출력

--- D220208/b17142연구소3.java
+++ D220208/b17142연구소3.java
@@ -3,24 +3,40 @@ package studyAlgorithm.D220208;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class b17142연구소3 {
 
     static class virus{
+
+        virus(){}
+
         virus(int y, int x){
             this.y = y;
             this.x = x;
         }
 
+        virus(int y, int x, int score){
+            this.y =y;
+            this.x = x;
+            this.score =score;
+        }
+
         int x;
         int y;
+        int score;
     }
 
     static int[][] arrMap;
     static int[][] copyMap;
+    static boolean[][] visit;
+    static boolean[][] copyVisit;
     static ArrayList<virus> virusList;
-
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static virus[][] virusMap;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -32,7 +48,9 @@ public class b17142연구소3 {
         int M = Integer.parseInt(st.nextToken());
 
         arrMap = new int[N][N];
+        visit = new boolean[N][N];
         virusList = new ArrayList<virus>();
+        virusMap  = new virus[N][N];
 
         for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine(), " ");
@@ -58,6 +76,15 @@ public class b17142연구소3 {
     static void virusComb(int[] virus, boolean[] selectVirus, int startIdx, int virusCnt){
         if(virusCnt == 0){
             copyMap = arrMap;
+
+            for(int i = 0 ; i < arrMap.length ; i++){
+                for(int j = 0 ; j < arrMap.length ; j++){
+                    visit[i][j] = false;
+                    virusMap[i][j] = new virus(i,j,Integer.MAX_VALUE);
+                    virusMap[i][j].score = 99;
+                }
+            }
+
             process(selectVirus);
             return;
         }
@@ -72,6 +99,55 @@ public class b17142연구소3 {
     }
 
     static void process(boolean[] selectVirus){
+        for(int i = 0 ; i < selectVirus.length ; i++){
+            if(selectVirus[i] == true){
+                virusMap[virusList.get(i).y][virusList.get(i).x].score = 0;
+                visit = new boolean[arrMap.length][arrMap.length];
+                bfs(i);
+            }
+        }
 
+        for(int i = 0 ; i < virusMap.length ; i++){
+            for(int j = 0 ; j < virusMap[0].length ; j++){
+                System.out.print(virusMap[i][j].score + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    // 바이러스 퍼트리기
+    static void bfs(int idx){
+        Queue queue = new LinkedList();
+        virus virus = new virus();
+
+        virus.x = virusList.get(idx).x;
+        virus.y = virusList.get(idx).y;
+        virus.score = 0;
+        int score = 0;
+        queue.add(virus);
+        visit[virus.y][virus.x] = true;
+
+        while (!queue.isEmpty()) {
+            score++;
+
+            virus point = (virus)queue.poll();
+            //copyMap[point.y][point.x] = score;
+            visit[point.y][point.x] = true;
+
+            for(int i = 0 ; i < 4 ; i++){
+                int nextX = point.x + dx[i];
+                int nextY = point.y + dy[i];
+
+                if(0 <= nextX && 0 <= nextY){
+                    if(nextY < copyMap.length && nextX < copyMap[0].length){
+                        if(virusMap[nextY][nextX].score >= virusMap[point.y][point.x].score+1 && copyMap[nextY][nextX] == 0 && visit[nextY][nextX] == false){
+                            virusMap[nextY][nextX].score = virusMap[point.y][point.x].score+1;
+                            queue.add(new virus(nextY, nextX));
+                        }
+                    }
+                }
+            }
+        }
     }
 }
