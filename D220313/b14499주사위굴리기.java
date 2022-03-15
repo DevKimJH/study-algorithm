4db912eab72f0670d46934f3f64943bff1d0ed3a
KJH <hg116701@naver.com>
2022-03-15 18:27:08 +0900

    주사위 굴리기 1회차

--- D220313/b14499주사위굴리기.java
+++ D220313/b14499주사위굴리기.java
@@ -1,162 +1,113 @@
 package studyAlgorithm.D220313;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+public class b14499주사위굴리기{
 
-/**
- 크기가 N * M인 지도
- 지도의 오른쪽은 동쪽, 위쪽은 북쪽
- 지도의 좌표는 (r,c)
- r = 북쪽으로부터 떨어진 칸의 개수
- c = 서쪽으로부터 떨어진 칸의 개수
+    static int[] dice = new int[7];
 
- 2
- 4 1 3
- 5
- 6
-
- 지도 위에 윗 면이 1이고, 동쪽을 바라보는 방향이 3인 상태로 놓여져 있으며, 놓여져 있는 좌표는 (X,Y)
- 가장 처음에 주사위에는 모든 면에 0이 적혀져 있음
-
- 지도의 각 칸에는 정수가 하나씩 쓰여져 있다. 주사위를 구렸을 때, 이동하 칸에 쓰여있는 수가 0이면,
- 주사위의 바닥면에 쓰여 있는 수가 칸에 복사된다. 0이 아닌 경우에는 칸에 쓰여있는 수가 주사위의 바닥면으로 복사,
- 칸에 쓰여 있는 수는 0이 된다.
-
- 주사위를 놓는 곳의 좌표와 이동시키는 명령이 주어졌을 때, 주사위가 이동했을때마다 상단에쓰여 있는 값을 구하는 프로그램을 작성하시오
-
- 입력
- ---
- 첫째 줄
- 지도의 세로 크기 N
- 지도의 가로 크기 M
- 주사위를 놓는 곳의 좌표 X, Y
- 명령의 개수 K
-
- 둘째 줄
- N개의 줄에 지도에 쓰여 있는 수가 북쪽으로부터 남쪽으로, 각 죽을 서쪽부터 동쪽 순서대로 주어진다.
-
- 마지막 줄
- 이동하는 명령 동쪽1 서쪽 2 북쪽 3 남쪽 4
- */
-
-public class b14499주사위굴리기 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
         st = new StringTokenizer(br.readLine(), " ");
 
-        // 주사위
-        int[][] dice = new int[4][3];
-
-
-        // 지도 세로 크기
+        // 세로 크기
         int N = Integer.parseInt(st.nextToken());
 
-        // 지도 가로 크기
+        // 가로 크기
         int M = Integer.parseInt(st.nextToken());
 
-        // 주사위 놓는 좌표
-        int x = Integer.parseInt(st.nextToken());
+        // X좌표
         int y = Integer.parseInt(st.nextToken());
 
+        // Y좌표
+        int x = Integer.parseInt(st.nextToken());
+
         // 명령의 개수
-        int K = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         // 지도 세팅
-        int[][] arr = new int[N][M];
+        int[][] mapArr = new int[N][M];
+
         for(int i = 0 ; i < N ; i++){
-            st = new StringTokenizer(br.readLine(), " " );
-            for(int j = 0 ;j < M ; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0 ; j < M ; j++){
+                mapArr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+        st = new StringTokenizer(br.readLine(), " ");
 
         // 명령 세팅
-        st = new StringTokenizer(br.readLine(), " ");
-        Queue<Integer> queue = new LinkedList();
+        for(int i = 0 ; i < k ; i++){
+            int command = Integer.parseInt(st.nextToken());
 
-        for(int i = 0 ; i < K ; i++){
-            queue.add(Integer.parseInt(st.nextToken()));
+            // 1. 지도 범위 체크 후 주사위를 방향에 맞게 굴린다.
+            int nx = x + dx[command-1];
+            int ny = y + dy[command-1];
+
+            // 1-1. 이동할 범위가 지도를 벗어나지않으면
+            if(0 <= nx && nx < M && 0 <= ny && ny < N){
+                // 1-2. 주사위를 굴린다
+                rollDice(command);
+                x = nx;
+                y = ny;
+
+                // 2. 이동한 칸 숫자 확인
+                if(mapArr[y][x] == 0){
+                    // 2-1. 0이면 주사위 바닥 수가 지도로 복사
+                    mapArr[y][x] = dice[6];
+                }
+                else{
+                    // 2.2 0이 아니면 지도 칸에 쓰인 수가 주사위로 복사
+                    dice[6] = mapArr[y][x];
+
+                    // 2-3. 지도 칸은 0이 됨
+                    mapArr[y][x] = 0;
+                }
+
+                // 3. 이동이 끝나면 주사위 윗면 숫자 출력
+                System.out.println(dice[1]);
+            }
         }
+    }
 
-
-        // 시작
-        while(!queue.isEmpty()){
-            // 명령 뽑기
-            int inputCommand = queue.poll();
-            int tmp;
-            switch(inputCommand){
-                // 동쪽 이동
-                case 1:
-                    if(x+1 >= M){
-                        continue;
-                    }
-                    x++;
-                    tmp = dice[1][0];
-                    dice[1][0] = dice[3][1];
-                    dice[3][1] = dice[1][2];
-                    dice[1][2] = dice[1][1];
-                    dice[1][1] = tmp;
-                    break;
-                // 서쪽 이동
-                case 2:
-                    if(x-1 < 0){
-                        continue;
-                    }
-                    x--;
-                    tmp = dice[1][0];
-                    dice[1][0] = dice[1][1];
-                    dice[1][1] = dice[1][2];
-                    dice[1][2] = dice[3][1];
-                    dice[3][1] = tmp;
-
-                    break;
-                // 북쪽 이동
-                case 3:
-                    if(y-1 < 0){
-                        continue;
-                    }
-                    y--;
-                    tmp = dice[1][1];
-                    dice[1][1] = dice[2][1];
-                    dice[2][1] = dice[3][1];
-                    dice[3][1] = dice[0][1];
-                    dice[0][1] = tmp;
-                    break;
-                // 남쪽 이동
-                case 4:
-                    if(y+1 >= N) {
-                        continue;
-                    }
-                    y++;
-                    tmp = dice[1][1];
-                    dice[1][1] = dice[0][1];
-                    dice[0][1] = dice[3][1];
-                    dice[3][1] = dice[2][1];
-                    dice[2][1] = tmp;
-                    break;
-            }
-
-            // 이동한 칸에 쓰여 있는 수가 0이면, 주사위 바닥면에 쓰여 있는 수가 칸에 복사
-            // 0이 아닌 경우 칸에 쓰여 있는 수가 주사위의 바닥면으로 복사. 칸에 쓰여 있는 수는 0이 된다.
-
-            if(arr[y][x] == 0){
-                arr[y][x] = dice[3][1];
-            }
-            else{
-                dice[3][1] = arr[y][x];
-                arr[y][x] = 0;
-            }
-
-            // 주사위 상단에 쓰여 있는 값 출력
-            System.out.println(dice[1][1]);
+    static void rollDice(int command){
+        int[] temp = dice.clone();
+        switch(command){
+            // 동
+            case 1:
+                dice[1] = temp[4];
+                dice[3] = temp[1];
+                dice[4] = temp[6];
+                dice[6] = temp[3];
+                break;
+            // 서
+            case 2:
+                dice[1] = temp[3];
+                dice[3] = temp[6];
+                dice[4] = temp[1];
+                dice[6] = temp[4];
+                break;
+            // 북
+            case 3:
+                dice[1] = temp[5];
+                dice[2] = temp[1];
+                dice[5] = temp[6];
+                dice[6] = temp[2];
+                break;
+            // 남
+            case 4:
+                dice[1] = temp[2];
+                dice[2] = temp[6];
+                dice[5] = temp[1];
+                dice[6] = temp[5];
+                break;
         }
     }
 }
-
