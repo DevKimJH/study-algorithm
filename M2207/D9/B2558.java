a0956f743026e1df6995c04d546c6c083f5a40ac
KJH <hg116701@naver.com>
2022-07-09 21:18:13 +0900

    A+B

---
+++ M2207/D9/B2558.java
@@ -0,0 +1,16 @@
+package studyAlgorithm.M2207.D9;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B2558 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.print(A+B);
+    }
+}
+     
