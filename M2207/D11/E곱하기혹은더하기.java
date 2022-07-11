ba28eac7c2ab55a51408cbd74d717a73da787f6f
KJH <hg116701@naver.com>
2022-07-11 22:49:10 +0900

    이코테 곱하기 혹은 더하기

---
+++ M2207/D11/E곱하기혹은더하기.java
@@ -0,0 +1,14 @@
+package studyAlgorithm.M2207.D11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class E곱하기혹은더하기 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        
+    }
+}
