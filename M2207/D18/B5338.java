65959b683de4275b80f31308f43f5b496a7a2328
KJH <hg116701@naver.com>
2022-07-18 23:30:04 +0900

    마소로고

---
+++ M2207/D18/B5338.java
@@ -0,0 +1,12 @@
+package studyAlgorithm.M2207.D18;
+
+public class B5338 {
+
+    public static void main(String[] args) throws Exception{
+        System.out.print("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
