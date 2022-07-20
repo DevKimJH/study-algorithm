5fecfcf7a48f55ce478cedaedcbab7e4303389da
KJH <hg116701@naver.com>
2022-07-20 23:14:25 +0900

    no message

---
+++ M2207/D20/B5337.java
@@ -0,0 +1,9 @@
+package studyAlgorithm.M2207.D20;
+
+public class B5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
