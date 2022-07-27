254319fa7421817f96489bd54af3f2d66a18bb56
KJH <hg116701@naver.com>
2022-07-27 13:38:04 +0900

    no message

---
+++ M2207/D27/B10718.java
@@ -0,0 +1,8 @@
+package studyAlgorithm.M2207.D27;
+
+public class B10718 {
+    public static void main(String[] args) {
+        System.out.print("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+}
