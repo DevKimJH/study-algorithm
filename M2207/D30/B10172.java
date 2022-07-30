59d8d5bc8996275e75e038eea27c7ae31968f62c
KJH <hg116701@naver.com>
2022-07-30 20:44:23 +0900

    no message

---
+++ M2207/D30/B10172.java
@@ -0,0 +1,15 @@
+package studyAlgorithm.M2207.D30;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B10172 {
+    public static void main(String[] args) {
+        System.out.print("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+
+    }
+}
